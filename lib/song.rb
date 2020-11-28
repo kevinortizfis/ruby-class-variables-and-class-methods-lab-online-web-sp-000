917a2818da0088e8e062cefc570bf51fabdbe516
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-11-28 00:12:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   end
 
   def self.artist_count
-    wf = Hash.new(0).tap { |h| @@artists.uniq.each { |word| h[@@count] += 1 } }
+    wf = Hash.new(0).tap { |h| @@artists.uniq.each { |thing| h[word] += 1 } }
   end
 
 end
